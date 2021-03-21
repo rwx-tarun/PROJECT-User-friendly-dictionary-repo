0c60bf4e4b5f0e41d3be64e6956b672f42b64a3e
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-03-21 08:42:18 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,36 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+
+def translate(w):
+    w.lower()
+    if w in data:
+        return data[w]
+
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y or N :" %
+                   get_close_matches(w, data.keys())[0])
+
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+
+        elif yn == "N":
+            return "word doesn't exist"
+
+        else:
+            return "We didn't understand your input."
+
+    else:
+        return "Word doesn't exist. Please double check."
+
+
+word = input("enter word : ")
+output = print(translate(word))
+
+if(type(output) == list):
+    for item in output:
+        print(item)
+else:
+    print(output)
